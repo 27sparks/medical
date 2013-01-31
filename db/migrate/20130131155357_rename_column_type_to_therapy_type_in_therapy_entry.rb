b4a7edab68396b33dbcaa56371fed9cb7a73383b
Eric Nordmeyer <en@v2a.net>
2013-01-31 21:25:31 +0100

    fix migration

--- db/migrate/20130131155357_rename_column_type_to_therapy_type_in_therapy_entry.rb
+++ db/migrate/20130131155357_rename_column_type_to_therapy_type_in_therapy_entry.rb
@@ -1,11 +1,9 @@
 class RenameColumnTypeToTherapyTypeInTherapyEntry < ActiveRecord::Migration
   def up
     add_column :therapy_entries, :therapy_type, :string
-    remove_column :therapy_entries, :type, :string
   end
 
   def down
     remove_column :therapy_entries, :therapy_type, :string
-    add_column :therapy_entries, :type, :string
   end
 end
